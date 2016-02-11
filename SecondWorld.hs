7f738a123eac06cf330ff30a08125eb8365dbc85
bjornakr <bjornakr@gmail.com>
2016-02-11 16:11:39 +0100

    Added pick up action.

--- SecondWorld.hs
+++ SecondWorld.hs
@@ -1,14 +1,16 @@
 import Data.List
 
 type Id = String
+type Inventory = [Item]
+
 class IdEq a where
     idEq :: a -> a -> Bool
     getId :: a -> Id
     idEq a1 a2 = getId a1 == getId a2
 
-data Actor = Actor Id deriving (Show, Eq)
+data Actor = Actor Id [Item] deriving (Show, Eq)
 instance IdEq Actor where
-    getId (Actor id) = id
+    getId (Actor id _) = id
 
 data Item = Item Id deriving (Show, Eq)
 instance IdEq Item where
@@ -44,8 +46,23 @@ addItem (World rooms) room item = World (updateEntity (addItemToRoom room item) 
 addItemToRoom :: Room -> Item -> Room
 addItemToRoom (Room rId rs items as) item = Room rId rs (addOrUpdateEntity item items) as
 
--- Add roomcontent
+removeItemFromRoom :: Room -> Item -> Room
+removeItemFromRoom (Room rId rs items as) item = (Room rId rs (removeEntity item items) as)
 
+addItemToInventory :: Actor -> Item -> Actor
+addItemToInventory (Actor id items) item = (Actor id (item:items))
+
+
+transferItemFromRoomToActor :: World -> Item -> Room -> Actor -> World
+transferItemFromRoomToActor world item room@(Room _ _ items actors) actor
+    | roomHasItem && actorIsInRoom = addRoom world (removeItemFromRoom (addActorToRoom room (addItemToInventory actor item)) item)
+    | otherwise = world
+    where
+        roomHasItem = findEntity item items == Just item
+        actorIsInRoom = findEntity actor actors == Just actor
+
+-- transferActorToAnotherRoom :: World -> Room -> Room -> Actor -> World
+-- TODO: IMPLEMENT
 
 -- ==== ENTITY FUNCTIONS ====
 addOrUpdateEntity :: (IdEq a, Eq a) => a -> [a] -> [a]
@@ -68,13 +85,34 @@ updateEntity ue (e:es)
     | otherwise = e:(updateEntity ue es)
 
 
+removeEntity :: IdEq a => a -> [a] -> [a]
+removeEntity e [] = []
+removeEntity e' (e:es)
+    | idEq e' e = es
+    | otherwise = e:(removeEntity e' es)
+
 -- ==== TESTS ====
-actor1 = Actor "Actor 1"
+item1 = Item "Item1"
+actor1 = Actor "Actor1" []
 room1 = Room "Room1" [] [] []
 room2 = Room "Room2" [] [] []
 room3 = Room "Room3" [] [] []
 updatedRoom1 = Room "Room1" [room2] [] []
 world = World [room1, room2]
+
+transferItemFromRoomToActor_itemInSameRoomAsActor_itemIsTransfered =
+    let room = Room "Room1" [] [item1] [actor1] in
+    transferItemFromRoomToActor (World [room]) item1 room actor1 ==
+        World [Room "Room1" [] [] [(Actor "Actor1" [item1])]]
+transferItemFromRoomToActor_itemIsNotInRoom_nothingHappens =
+    let room = Room "Room1" [] [] [actor1] in
+    transferItemFromRoomToActor (World [room]) item1 room actor1 ==
+        World [Room "Room1" [] [] [(Actor "Actor1" [])]]
+transferItemFromRoomToActor_actorIsNotInRoom_nothingHappens =
+    let room = Room "Room1" [] [item1] [] in
+    transferItemFromRoomToActor (World [room]) item1 room actor1 ==
+        World [Room "Room1" [] [item1] []]
+
 
 addActorToRoom_noActorsInRoom_returnsRoomWithActor =
     addActorToRoom room1 actor1 == Room "Room1" [] [] [actor1]
